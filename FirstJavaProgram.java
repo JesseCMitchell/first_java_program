d5fe8df30b4df3c82f5cb43aa00a4e8b802853b3
Jesse Mitchell <jesse@jmitchellmusic.com>
2022-06-05 21:55:57 -0700

    initial commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+	public static void main(String[] args) {
+		System.out.println("My Name Is Jesse Mitchell");
+        System.out.println("I am 38 years old");
+        System.out.println("My hometown is Phoenix, AZ");
+	}
+}
